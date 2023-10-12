26af787f4d9befe70d0ee383cc693a2b1ff9080e
Aishwarya <sanku.aishwarya@feuji.com>
2023-10-12 10:25:33 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,145 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const Harbinger());
+
+main() {
+  runApp(const MyApp());
 }
 
-class Harbinger extends StatelessWidget {
-  const Harbinger({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      title: _title,
+      home: MyHomePage(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  final List<Widget> _screens = [
-    // Content for Home tab
-    Container(
-      color:  Colors.white10,
-      alignment: Alignment.center,
-      child: const Text(
-        'Workspace',
-        style: TextStyle(fontSize: 40),
-      ),
-    ),
-    // Content for Feed tab
-    Container(
-      color: Colors.purple.shade100,
-      alignment: Alignment.center,
-      child: const Text(
-        'Project',
-        style: TextStyle(fontSize: 40),
-      ),
-    ),
-    // Content for Favorites tab
-    Container(
-      color: Colors.red.shade100,
-      alignment: Alignment.center,
-      child: const Text(
-        'Test Plan',
-        style: TextStyle(fontSize: 40),
-      ),
-    ),
-    // Content for Settings tab
-    Container(
-      color: Colors.pink.shade300,
-      alignment: Alignment.center,
-      child: const Text(
-        'Settings',
-        style: TextStyle(fontSize: 40),
-      ),
-    )
-  ];
-
-  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 244, 151, 45),
-        title: const Center(
-          child: Text(
-            "Harbinger",
-          ),
-        ),
-      ),
-      bottomNavigationBar: MediaQuery.of(context).size.width < 640
-          ? BottomNavigationBar(
-              currentIndex: _selectedIndex,
-              unselectedItemColor: Colors.grey,
-              selectedItemColor: Colors.indigoAccent,
-              // called when one tab is selected
-              onTap: (int index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-              // bottom tab items
-              items: const [
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home), label: 'Workspace'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.feed), label: 'Project'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.view_agenda), label: 'Test'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.settings), label: 'Settings')
-                ])
-          : null,
-      body: Row(
-        children: [
-          if (MediaQuery.of(context).size.width >= 640)
-            NavigationRail(
-              onDestinationSelected: (int index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-              selectedIndex: _selectedIndex,
-              destinations: const [
-                NavigationRailDestination(
-                    icon: Icon(Icons.home), label: Text('Workspace')),
-                NavigationRailDestination(
-                    icon: Icon(Icons.feed), label: Text('Project')),
-                NavigationRailDestination(
-                    icon: Icon(Icons.favorite), label: Text('Favorites')),
-                NavigationRailDestination(
-                    icon: Icon(Icons.settings), label: Text('Settings')),
-              ],
-
-              labelType: NavigationRailLabelType.all,
-              selectedLabelTextStyle: const TextStyle(
-                color: Colors.teal,
-              ),
-
-              unselectedLabelTextStyle: const TextStyle(),
-              // Called when one tab is selected
-              leading: const Column(
-                children: [
-                  SizedBox(
-                    height: 8,
-                  ),
-                  CircleAvatar(
-                    radius: 20,
-                    child: Icon(Icons.person),
-                  ),
-                ],
-              ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          leading: Icon(Icons.sunny, color: Colors.white),
+          title:
+              const Text('Flutter Demo', style: TextStyle(color: Colors.white)),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(Icons.search, color: Colors.white),
             ),
-          Expanded(child: _screens[_selectedIndex])
-        ],
+            Padding(
+              padding: const EdgeInsets.only(left: 8, right: 8),
+              child: Icon(Icons.more_vert, color: Colors.white),
+            )
+          ],
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(
+              child: Text("just a button"),
+              onPressed: () => {},
+            ),
+            Card(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                height: MediaQuery.of(context).size.height * .3,
+                child: Image.network(
+                  "https://picsum.photos/200/300",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
